88f37447ea9cf7d35db06db9f2cb62de0f88619c
wdomer <ebrahimomer73@gmail.com>
2020-09-25 09:44:09 +0300

    change password screen

---
+++ lib/models/change_password_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'change_password_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ChangePasswordModel _$ChangePasswordModelFromJson(Map<String, dynamic> json) {
+  return ChangePasswordModel(
+    password: json['password'] as String,
+  );
+}
+
+Map<String, dynamic> _$ChangePasswordModelToJson(
+        ChangePasswordModel instance) =>
+    <String, dynamic>{
+      'password': instance.password,
+    };
